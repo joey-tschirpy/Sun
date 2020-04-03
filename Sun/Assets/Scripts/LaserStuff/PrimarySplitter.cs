ed8df1c57717c6fa73bd2d294267ee99a6637ad3
joe-tschirpig <joe.tschirpig@mightykingdom.com>
2020-04-04 09:30:14 +1030

    Add Functionality to PrimarySplitter: - setup / manipulation of lasers

--- Sun/Assets/Scripts/LaserStuff/PrimarySplitter.cs
+++ Sun/Assets/Scripts/LaserStuff/PrimarySplitter.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class PrimarySplitter : LaserObject
 {
+    protected override void Awake()
+    {
+        base.Awake();
 
+        var module = GetModule(ModuleType.ManipulationInput);
+        if (module != null)
+        {
+            var input = (ManipulationInputModule)module;
+            input.manipulateFunction += ManipulateLaser;
+        }
+    }
+
+    private Laser[] ManipulateLaser(Laser combinedInput)
+    {
+        var lasers = new Laser[]
+        {
+            Laser.Filter(combinedInput, LaserColor.Red),
+            Laser.Filter(combinedInput, LaserColor.Green),
+            Laser.Filter(combinedInput, LaserColor.Blue)
+        };
+
+        foreach (var laser in lasers)
+        {
+            if (laser.Color == LaserColor.Black)
+            {
+                laser.Power = 0;
+            }
+        }
+
+        return lasers;
+    }
 }
